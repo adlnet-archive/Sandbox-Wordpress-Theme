65496f1aaa523296b0f0f180518f01eaa0923ea3
Mick Muzac <mmuzac23@gmail.com>
2013-12-12 11:18:37 -0500

    Added basic navigation links to single posts

--- single.php
+++ single.php
@@ -10,8 +10,28 @@
 get_header(); ?>
 <div class="content container">	
 	<?php while ( have_posts() ) : the_post(); ?>
+	
+		<?php
+			ob_start(); 
+			previous_post_link('%link', '<  %title', true); 
+			$previous = ob_get_contents();
+			ob_clean();
+			next_post_link('%link', '%title  >', true); 
+			$next = ob_get_contents();
+			ob_end_clean();
+		?>
+		<div class="navContainer">
+			<div class="previousLink">
+				<?php echo $previous ? $previous : ''; ?>
+			</div>
+			<div class="nextLink">
+			<?php	echo $next ? $next : ''; ?>
+			</div>
+		</div>
 
 		<h1><?php echo the_title(); ?></h1>
+		
+
 
 		<div class="the_post"><?php echo the_content(); ?></div>
 
