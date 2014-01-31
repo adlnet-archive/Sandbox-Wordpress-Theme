2b861ad97fd561373deba49a1d3ff8a46e8b3449
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2014-01-31 16:36:45 -0500

    Sundry fixes and patches

--- footer.php
+++ footer.php
@@ -4,10 +4,11 @@
  */
 ?>
 			<div id="footer" class="container">
-				<br/><p style="color:#eee">A project from <a href='http://www.adlnet.gov'>ADL</a></p>
-				<a href="<?php echo $root;?>/test">Test Browser Support</a> 
-			</div>
+				<br/><p>The views expressed on this website in the form of documentation, blog articles, or tutorials do not necessarily represent the views or policies of ADL or the U.S. Government.</p>
 
+				<p>Sponsored by the Office of the Under Secretary of Defense for Personnel and Readiness (OUSD P&amp;R).
+				This is an official website of the U.S. Government (c)2014 Advanced Distributed Learning (ADL).</p>
+			</div>
 		</div>
 	</div>
 	<a href="https://github.com/adlnet/Sandbox" id="gitBanner"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
